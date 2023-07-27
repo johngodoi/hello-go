8b5b3513df34850b6dbe6c45224cc88c6b0aa62b
John Henrique Teixeira de Godoi <john.godoi@gmail.com>
2023-07-27 00:03:13 -0300

    feat: setup db with some coins seed

--- scripts/sql/coins.sql
+++ scripts/sql/coins.sql
@@ -1,1 +1,5 @@
 create table if not exists coins (name char(40), description char(120));
+
+insert into coins (name, description) values 
+('BITCOIN', 'First cryptocurrency to solve double spend problem and be largely adopted'),
+('ETHEREUM', 'Cryptocurrency that supports a variety dapp creation');
